43d44ff0b43f43299ad14c00f26a75aa9b0bdd97
DarioMachado <115183065+DarioMachado@users.noreply.github.com>
2024-01-09 14:19:17 +0100

    Add files via upload

---
+++ PMDM/android9enero/app/src/main/java/com/example/android9enero/ejercicios/Guidelines.kt
@@ -0,0 +1,12 @@
+package com.example.android9enero.ejercicios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android9enero.R
+
+class Guidelines : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guidelines)
+    }
+}
