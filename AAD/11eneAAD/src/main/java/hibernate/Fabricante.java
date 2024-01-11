7b2aaf12f58731ac4197b3c6d407303eaf1c6267
DarioMachado <115183065+DarioMachado@users.noreply.github.com>
2024-01-11 11:09:46 +0100

    Add files via upload

---
+++ AAD/11eneAAD/src/main/java/hibernate/Fabricante.java
@@ -0,0 +1,59 @@
+package hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "fabricante")
+public class Fabricante {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "nombre")
+	private String nombre;
+
+	public Fabricante(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Fabricante [id=" + id + ", nombre=" + nombre + "]";
+	}
+	
+	
+}
+
+
+/*
+@Entity
+public record Fabricante(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) int id, String nombre) {
+	
+}*/
+
+
---
+++ AAD/11eneAAD/src/main/java/hibernate/Principal.java
@@ -0,0 +1,38 @@
+package hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.context.internal.ThreadLocalSessionContext;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		
+		ThreadLocalSessionContext context = new ThreadLocalSessionContext((SessionFactoryImplementor)sessionFactory);
+		
+		context.bind(sessionFactory.openSession());
+		
+		try {
+		
+		Session session = context.currentSession();
+		
+		session.beginTransaction();
+		
+		Fabricante fabricante = new Fabricante(222, "hola");
+		
+		session.save(fabricante);
+		
+		session.getTransaction().commit();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	
+	}
+
+}
