f0b69b925ce20a4a3b74999111d0c3895bcfddc0
DarioMachado <115183065+DarioMachado@users.noreply.github.com>
2024-01-22 11:15:25 +0100

    Add files via upload

--- PMDM/EjerciciosIntermediosAndroid/app/src/main/java/com/example/ejerciciosintermediosandroid/Ej2CardView.kt
+++ PMDM/EjerciciosIntermediosAndroid/app/src/main/java/com/example/ejerciciosintermediosandroid/Ej2CardView.kt
@@ -2,10 +2,22 @@ package com.example.ejerciciosintermediosandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.slider.RangeSlider
 
 class Ej2CardView : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ej2_card_view)
+
+        val rangeSlider: RangeSlider = findViewById<RangeSlider>(R.id.rangoEdad)
+        val tvEdad: TextView = findViewById<TextView>(R.id.tvEdad) //Qué difícil era, no?
+        rangeSlider.addOnChangeListener { _, value, fromUser ->
+            tvEdad.text = "${value.toInt()} AÑOS"
+        }
+
     }
 }
