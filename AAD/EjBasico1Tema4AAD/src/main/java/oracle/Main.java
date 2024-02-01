f50c2fc060c0eeab84a1d8379817a24c14112441
DarioMachado <115183065+DarioMachado@users.noreply.github.com>
2024-02-01 09:23:48 +0100

    Add files via upload

--- AAD/EjBasico1Tema4AAD/src/main/java/oracle/Main.java
+++ AAD/EjBasico1Tema4AAD/src/main/java/oracle/Main.java
@@ -1,9 +1,14 @@
 package oracle;
 
+import java.util.List;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.context.internal.ThreadLocalSessionContext;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+
+import oracle.Empresa;
 
 public class Main {
 
@@ -13,6 +18,13 @@ public class Main {
 			ThreadLocalSessionContext context = new ThreadLocalSessionContext((SessionFactoryImplementor) sessionFactory);
 			context.bind(sessionFactory.openSession());
 			
+			Session session = context.currentSession();
+			
+			List<Empresa> empresas = session.createQuery("from Empresa", Empresa.class).list();
+			
+			for(Empresa empresa: empresas) {
+				System.out.println(empresa);
+			}
 			
 	}
 	
